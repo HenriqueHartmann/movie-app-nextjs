2ad12427f4c342637563958a7bd57a0d588a2c6f
HenriqueHartmann <alemaohartmann@gmail.com>
2025-04-02 20:08:49 -0300

    main - update - update Home page, add error handling, add alert throw, add image to item by using poster_path field from results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,61 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import HttpRequest from "@/services/HttpRequest"
 import Loading from "@/components/Loading/loading"
+import HttpError from "@/components/Error/http-error"
+import Alert from "@/components/Alert/alert"
 import MovieItem from "@/components/MovieItem/movie-item"
 import MovieItemParams from "@/types/Movie/movie-item-params"
 import "@/styles/home/home.css"
+import HttpErrorComponentParams from "@/types/Error/http-error-component-params"
 
 function Home() {
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [showAlert, setShowAlert] = useState<boolean>(false)
     const [movieList, setMovieList] = useState<MovieItemParams[]>([])
+    const [hasError, setHasError] = useState<boolean>(false);
+    const [errorData, setErrorData] = useState<HttpErrorComponentParams | null>(null)
+    const [alertMessage, setAlertMessage] = useState<string>("Something went wrong!")
 
     useEffect(() => {
-        function formatResponse(list: []): MovieItemParams[] {
-            return list.map(item => ({
-                movieTitle: item["title"],
-                movieYear: item["release_date"],
-                movieImageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s" // key: poster_path
-            }));
-        }
-    
         function getApiKey(): string {
             return process.env.NEXT_PUBLIC_MOVIE_API_KEY ?? ""
         }
 
-        async function getMovies() {
-            const url = 'https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&sort_by=popularity.desc'
+        function getPosterPath(posterPath: string | null ): string {
+            const imageApiBaseUrl = "https://image.tmdb.org/t/p/w500/"
 
+            if (posterPath) {
+                return imageApiBaseUrl + posterPath
+            }
+
+            return "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s"
+        }
+
+        function setFormatResponse(list: []): void {
+            const formattedList = list.map(item => ({
+                movieTitle: item["title"],
+                movieYear: item["release_date"],
+                movieImageSrc: getPosterPath(item["poster_path"]) // key: poster_path
+            }));
+
+            setMovieList(formattedList)
+        }
+
+        function showAlertComponent(): void {
+            setShowAlert(true)
+            setTimeout(() => {
+                setShowAlert(false)
+            }, 3000)
+        }
+
+        async function getMovies() {
             try {
                 setIsLoading(true)
+                setMovieList([])
+                setHasError(false)
+                setErrorData(null)
 
+                const url = 'https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&sort_by=popularity.desc'
                 const options = {
                     headers: {
                         Authorization: `Bearer ${getApiKey()}`,
@@ -35,12 +63,26 @@ function Home() {
                     },
                 }
 
-                const res = await HttpRequest.get(url, options)
-                const formatedRes = formatResponse(res['results'])
+                await new Promise(resolve => setTimeout(resolve, 2000))
 
-                setMovieList(formatedRes)
-            } catch (error) {
-                console.log(error)
+                const res = await HttpRequest.get(url, options)
+                setFormatResponse(res['results'])
+            } catch (error: any) {
+                let message = "Something went wrong!"
+
+                if (error.body && error.body.status_message) {
+                    message = error.body.status_message
+                    setAlertMessage(message)
+                }
+
+                setErrorData({
+                    status: error.status,
+                    title: error.message,
+                    message: message,
+                    onClickCallback: showAlertComponent,
+                })
+                setHasError(true)
+                showAlertComponent()
             } finally {
                 setIsLoading(false)
             }
@@ -55,22 +97,22 @@ function Home() {
         </div>
     ))
 
-    if (isLoading) {
-        return (
-            <div className='body'>
-                <div className='content'>
-                    <Loading />
-                </div>
-            </div>
-        )
-    }
-
     return (
-        <div className='body'>
-            <div className='content'>
-                <div className='content-grid'>
-                    {movieListItems}
-                </div>
+        <div className="body">
+            {showAlert ? <Alert message={alertMessage} onClose={() => setShowAlert(false)} /> : null}
+            <div className="content">
+                {isLoading ? (
+                    <Loading />
+                ) : hasError && errorData ? (
+                    <HttpError
+                        status={errorData.status}
+                        title={errorData.title}
+                        message={errorData.message}
+                        onClickCallback={errorData.onClickCallback}
+                    />
+                ) : (
+                    <div className="content-grid">{movieListItems}</div>
+                )}
             </div>
         </div>
     )
