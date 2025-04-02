704c79bc415e7227a429c4cf7f2e7c33fa8c7c1a
HenriqueHartmann <alemaohartmann@gmail.com>
2025-04-02 02:05:06 -0300

    main - update - update home page, insert functions (getApiKey, formatResponse) inside useEffect and other small things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
+import HttpRequest from "@/services/HttpRequest"
 import Loading from "@/components/Loading/loading"
 import MovieItem from "@/components/MovieItem/movie-item"
 import MovieItemParams from "@/types/Movie/movie-item-params"
 import "@/styles/home/home.css"
-import HttpRequest from "@/services/HttpRequest"
 
 function Home() {
     const [isLoading, setIsLoading] = useState<boolean>(true)
     const [movieList, setMovieList] = useState<MovieItemParams[]>([])
 
-    function formatResponse(list: []): MovieItemParams[] {
-        return list.map(item => ({
-            movieTitle: item["title"],
-            movieYear: item["release_date"],
-            movieImageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s" // key: poster_path
-        }));
-    }
-
-    function getApiKey(): string {
-        return process.env.NEXT_PUBLIC_MOVIE_API_KEY!
-    }
-
     useEffect(() => {
+        function formatResponse(list: []): MovieItemParams[] {
+            return list.map(item => ({
+                movieTitle: item["title"],
+                movieYear: item["release_date"],
+                movieImageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s" // key: poster_path
+            }));
+        }
+    
+        function getApiKey(): string {
+            return process.env.NEXT_PUBLIC_MOVIE_API_KEY ?? ""
+        }
+
         async function getMovies() {
             const url = 'https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&sort_by=popularity.desc'
 
